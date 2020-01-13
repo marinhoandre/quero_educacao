6aaab2b9d808a71b5251b154dd5ea87ac6f5abdb
Flávio André <flaviomarinhoandre@gmail.com>
2020-01-12 23:26:42 -0300

    Carga

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Quero Education</title>
+    <title>Quero Educação</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
     <div class="header clearfix">
-        <h3 class="text-muted">Quero Education - Avaliação Técnica</h3>
+        <h3 class="text-muted">Quero Educação - Avaliação Técnica</h3>
     </div>
     <hr />
     <div class="alert alert-secondary">
